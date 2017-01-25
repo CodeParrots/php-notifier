76890858afeb737353503ceedc7260d0c941cdbb
Ben Rothman <brothman01@gmail.com>
2017-01-25 14:50:17 -0500

    started the settings page

--- php-notifier.php
+++ php-notifier.php
@@ -229,3 +229,60 @@ class CP_PHP_Notifier {
 }
 
 $cp_php_notifier = new CP_PHP_Notifier();
+
+
+
+
+/* Options Page */
+
+
+// create custom plugin settings menu
+add_action('admin_menu', 'my_cool_plugin_create_menu');
+
+function my_cool_plugin_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('My Cool Plugin Settings', 'PHP Notifier', 'administrator', __FILE__, 'my_cool_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
+
+	//call register settings function
+	add_action( 'admin_init', 'register_my_cool_plugin_settings' );
+}
+
+
+function register_my_cool_plugin_settings() {
+	//register our settings
+	register_setting( 'pn-email-enabled', 'new_option_name' );
+	register_setting( 'pn-periodic-emails', 'some_other_option' );
+	register_setting( 'pn-periodic-email-value', 'option_etc' );
+}
+
+function my_cool_plugin_settings_page() {
+?>
+<div class="wrap">
+<h1>Your Plugin Name</h1>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'my-cool-plugin-settings-group' ); ?>
+    <?php do_settings_sections( 'my-cool-plugin-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Send Email to Admin?</th>
+        <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">Send Emails periodically?</th>
+        <td><input type="checkbox" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
+        </tr>
+
+        <tr valign="top">
+        <th scope="row">How Frequently should Emails be sent?.</th>
+        <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
+        </tr>
+    </table>
+
+    <?php submit_button(); ?>
+
+</form>
+</div>
+<?php } ?>
